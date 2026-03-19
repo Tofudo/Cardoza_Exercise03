c0ed506ed25b6ba5ed3639c2c689a74cfd31ae33
Shem <shemcardoza7@gmail.com>
2026-03-19 15:42:18 +0800

    Exercise03 Activity A

--- app/src/main/java/com/example/cardoza_exercise03/MainActivity.java
+++ app/src/main/java/com/example/cardoza_exercise03/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.cardoza_exercise03;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button myButton;
+    EditText etName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,19 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        myButton = findViewById(R.id.btnSubmit);
+        etName = findViewById(R.id.etName);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = etName.getText().toString();
+                Intent intent = new Intent(MainActivity.this, GreetingActivity.class);
+                intent.putExtra("USER_NAME", name); // "USER_NAME" is the key
+                startActivity(intent);
+            }
         });
     }
 }
